7db84edf5d4df55823d1d62fffa772e7e4b3a31d
penguinflipper <astressertsa@outlook.com>
2024-02-03 22:09:00 +0000

    added Rest

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,13 +1,29 @@
 import os
 from flask import Flask
+from flask_restful import Resource, Api
+
+info = {
+    'i1': {'title': 'the boys'},
+    'i2': {'title': 'test titles'}
+}
+
+class TestRes(Resource):
+    
+    def get(self, infoId):
+        # return "Test!!!"
+        if infoId == "":
+            return info
+        return info[infoId]
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    api = Api(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
     )
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -24,9 +40,11 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    @app.route('/')
-    def hello():
-        return '<h1>Hello, World!<h1>'
+    # @app.route('/hello')
+    # @app.route('/')
+    # def hello():
+    #     return '<h1>Hello, World!<h1>'
+
+    api.add_resource(TestRes, '/main/<infoId>')
 
     return app
